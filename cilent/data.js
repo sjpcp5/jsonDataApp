3e2ff98e5daba32bd0235d54e578613787edce83
Saphirah <sjpcp5@gmail.com>
2021-07-22 16:35:25 -0500

    create react app create data.js file and populate file add git ignore file and add node modules

---
+++ cilent/data.js
@@ -0,0 +1,360 @@
+export const cardData = [
+  {
+    id: "608c140c91b0756556912438",
+    index: 0,
+    guid: "0192b8dc-7dc6-413a-84ce-69dcb0ffacb0",
+    isActive: true,
+    project: "Dam Break Studies",
+    picture: "images/damfailure.jpg",
+    links: [
+      {
+        pointsOfContact:
+          "https://cops.usace.army.mil/sites/HHC/CoPs/DS/MMC/Lists/MMC%20Points%20of%20Contact/AllItems.aspx?InitialTabId=Ribbon%2EListItem&VisibilityContext=WSSTabPersistence",
+        factSheet:
+          "https://cops.usace.army.mil/sites/HHC/CoPs/DS/MMC/Shared%Documents/MMC%Fact%Sheets/FactSheet_3_Products_FINAL_2015-03-01.pdf",
+        statusReport:
+          "https://cops.usace.army.mil/sites/HHC/CoPs/DS/MMC/MMC%Project%Status/DamProjects-Completed-29May2015.pdf",
+        technicalManual:
+          "https://cops.usace.army.mil/sites/HHC/CoPs/DS/MMC/Shared%Documents/MMC%Standard%Operating%Procedures/Current-2015/FY2015-MMC-Technical-Manual-for-Dams-Final.pdf",
+      },
+    ],
+    description:
+      "Provide training and produce dam breach analyses for flood control dams.  Includes numerical modeling using HEC-RAS 5.0 with 2D capability, inundation maps for emergency plans, and consequence estimation.  Screen navigation dams for potential consequences resulting from a breach.",
+  },
+  {
+    id: "608c140c81846e2feec1ec00",
+    index: 1,
+    guid: "caacb05f-fd97-4028-9c8e-3a6d6c27409d",
+    isActive: true,
+    project: "Levee Overtop / Breach Studies",
+    picture: "images/leveeovertop.jpg",
+    links: [
+      {
+        pointsOfContact:
+          "https://cops.usace.army.mil/sites/HHC/CoPs/DS/MMC/Lists/MMC%20Points%20of%20Contact/AllItems.aspx?InitialTabId=Ribbon%2EListItem&VisibilityContext=WSSTabPersistence",
+        statusReport:
+          "https://cops.usace.army.mil/sites/HHC/CoPs/DS/MMC/MMC%Project%Status/Levees%Completed.pdf",
+        technicalManual:
+          "https://cops.usace.army.mil/sites/HHC/CoPs/DS/MMC/Shared%Documents/MMC%Standard%Operating%Procedures/Current-2015/FY2015-MMC-Technical-Manual-For-Levees-Draft.pdf",
+      },
+    ],
+    description:
+      "Provide training and produce levee overtop/breach analyses for standard loading scenarios. Includes numerical modeling using HEC-RAS 5.0 with 2D capability, inundation maps for emergency plans, and consequence estimation.",
+  },
+  {
+    id: "608c140c18174567a471782c",
+    index: 2,
+    guid: "7b430a1b-cc38-48cf-8b34-59a5362fbcd9",
+    isActive: true,
+    project: "CWMS Watershed Models",
+    picture: "images/cwms.png",
+    links: [
+      {
+        pointsOfContact:
+          "https://cops.usace.army.mil/sites/HHC/CoPs/DS/MMC/Lists/MMC%20Points%20of%20Contact/AllItems.aspx?InitialTabId=Ribbon%2EListItem&VisibilityContext=WSSTabPersistence",
+        factSheet:
+          "https://cops.usace.army.mil/sites/HHC/CoPs/DS/MMC/Shared%Documents/MMC%Fact%Sheets/FactSheet_7_CorpsWaterManagementProgram_CAO_2015_03_01.pdf",
+        statusReport:
+          "https://cops.usace.army.mil/sites/HHC/CoPs/DS/MMC/MMC%Project%Status/CWMS%Projects%Completed.pdf",
+        technicalManual:
+          "https://cops.usace.army.mil/sites/HHC/CoPs/DS/MMC/Shared%Documents/MMC%Standard%Operating%Procedures/Current-2015/FY2015-MMC-Technical-Manual-for-CWMS-Final.pdf",
+      },
+    ],
+    description:
+      "The Corps Water Management System (CWMS) teams provide training and lead teams to develop HMS, Res-Sim, RAS and FIA models. Assist districts with implementation of CWMS once models are available.",
+  },
+  {
+    id: "608c140c58a312a0e395730c",
+    index: 3,
+    guid: "4384e83f-5c18-4227-a586-b9f7b314da79",
+    isActive: true,
+    project: "Hydrologic Hazard Studies",
+    picture: "images/extremestorm.jpg",
+    links: [
+      {
+        pointsOfContact:
+          "https://cops.usace.army.mil/sites/HHC/CoPs/DS/MMC/Lists/MMC%20Points%20of%20Contact/AllItems.aspx?InitialTabId=Ribbon%2EListItem&VisibilityContext=WSSTabPersistence",
+        sharePoint:
+          "https://cops.usace.army.mil/sites/HHC/CoPs/DS/MMC/Shared Documents/Hydrologic Hazards Team",
+        draftWorkPlan:
+          "https://cops.usace.army.mil/sites/HHC/CoPs/DS/MMC/Shared%Documents/Hydrologic%Hazards%Team/2015_04_17%-%Work%Plan%-%DRAFT%-%Stowasser.pdf",
+        statusReport:
+          "https://cops.usace.army.mil/sites/HHC/CoPs/DS/MMC/Shared%Documents/Hydrologic%Hazards%Team/2015_04_29%-%Hydrologic%Hazards%Status%-%Stowasser.xlsx",
+        hydroHazMap: "https://prod.mmc.usace.army.mil/mmc/statusmap/hht",
+      },
+    ],
+    description:
+      "Guidance, training, and production of the following tasks: gathering extreme storms, updating the probable maximum precipitation (PMP), developing the inflow design flood (IDF), routing the IDF to determine the probable maximum flood (PMF), studying paleo-flood hydrology, and determining hydrologic loading curves.",
+  },
+  {
+    id: "608c140cb3edfa08e54d0d00",
+    index: 4,
+    guid: "87774c28-63e3-41ac-b45b-98a1937be9a7",
+    isActive: true,
+    project: "Emergency Plan Maps",
+    picture: "images/eap_map.png",
+    links: [
+      {
+        pointsOfContact:
+          "https://cops.usace.army.mil/sites/HHC/CoPs/DS/MMC/Lists/MMC%20Points%20of%20Contact/AllItems.aspx?InitialTabId=Ribbon%2EListItem&VisibilityContext=WSSTabPersistence",
+        factSheet:
+          "https://cops.usace.army.mil/sites/HHC/CoPs/DS/MMC/Shared&Documents/MMC&Fact&Sheets/FactSheet_4_MappingOverview_CAO_2015_03_01.pdf",
+        statusReport:
+          "https://cops.usace.army.mil/sites/HHC/CoPs/DS/MMC/MMC&Project&Status/DamProjects-Completed-29May2015.pdf",
+        technicalManual:
+          "https://cops.usace.army.mil/sites/HHC/CoPs/DS/MMC/Shared&Documents/MMC&Standard&Operating&Procedures/Current-2015/FY2015-MMC-Technical-Manual-for-Dams-Final.pdf",
+      },
+    ],
+    description:
+      "Dam and Levee overtop/breach maps in multiple static and interactive formats for use in emergency plans.",
+  },
+  {
+    id: "608c140c0f91914db4b81aac",
+    index: 5,
+    guid: "d22751e9-4576-4b14-a4fd-d4c73b7eaecd",
+    isActive: true,
+    project: "Consequences Assessments",
+    picture: "images/hec-fia.png",
+    links: [
+      {
+        pointsOfContact:
+          "https://cops.usace.army.mil/sites/HHC/CoPs/DS/MMC/Lists/MMC%20Points%20of%20Contact/AllItems.aspx?InitialTabId=Ribbon%2EListItem&VisibilityContext=WSSTabPersistence",
+        factSheet:
+          "https://cops.usace.army.mil/sites/HHC/CoPs/DS/MMC/Shared%Documents/MMC%Fact%Sheets/FactSheet_5_Consequences_2015-03-01.pdf",
+      },
+    ],
+    description:
+      "HEC-FIA and other tools are used to estimate the consequences of flooding events. Consequences include potential life loss; structure, vehicle, and content damage; and lost project benefits. The Consequence Branch supports multiple MMC programs and also provides risk assessment expertise for both the dam safety and levee safety programs.",
+  },
+  {
+    id: "608c6afaa91d249ae7d586ef",
+    index: 6,
+    guid: "a9999c24-0dc9-4afb-adc8-2351bf1e4057",
+    isActive: true,
+    project: "Reports and Documentation",
+    picture: "images/docs.png",
+    links: [
+      {
+        pointsOfContact:
+          "https://cops.usace.army.mil/sites/HHC/CoPs/DS/MMC/Lists/MMC%20Points%20of%20Contact/AllItems.aspx?InitialTabId=Ribbon%2EListItem&VisibilityContext=WSSTabPersistence",
+        newsletter:
+          "https://cops.usace.army.mil/sites/HHC/CoPs/DS/MMC/Shared%Documents/Forms/AllItems.aspx?RootFolder=%2Fsites%2FHHC%2FCoPs%2FDS%2FMMC%2FShared%20Documents%2FMMC%20Newsletter&FolderCTID=0x012000448E47A8D6D89E4C845DEE97DCF4EC77&View=%7bE7B3449D-4ED7-409B-BD37",
+      },
+    ],
+    description:
+      "Dedicated team of technical staff, writers and editors. Finalize consequence assessment reports (CARs) and hydraulic modeling reports, publish SOPs, revise agency guidance and policy documents.",
+  },
+  {
+    id: "608c6afa3eee5c379252cc20",
+    index: 7,
+    guid: "87c28f42-0832-4686-9ba0-3b6f66c60bcc",
+    isActive: true,
+    project: "Flood Inundation Modeling and Mapping (FIM) Cadre",
+    picture: "images/fim.png",
+    links: [
+      {
+        pointsOfContact:
+          "https://cops.usace.army.mil/sites/HHC/CoPs/DS/MMC/Lists/MMC%20Points%20of%20Contact/AllItems.aspx?InitialTabId=Ribbon%2EListItem&VisibilityContext=WSSTabPersistence",
+        OPORD:
+          "https://cops.usace.army.mil/sites/HHC/CoPs/DS/MMC/Flood%Inundation%Modeling%FIM%Cadre/FIMcadreSummary-2014-11-AllHazardsOPORD.pdf",
+        factSheet:
+          "https://cops.usace.army.mil/sites/HHC/CoPs/DS/MMC/Flood%Inundation%Modeling%FIM%Cadre/factsheet_flood_inundation_mapping.pdf",
+        sharePoint:
+          "https://cops.usace.army.mil/sites/HHC/CoPs/DS/MMC/Flood&Inundation&Modeling&FIM&Cadre",
+        arcGISPortal:
+          "https://arcportal-ucop-corps.usace.army.mil/s0portal/apps/MapSeries/index.html?appid=807f9095c2ee48d3a80d1348ee4bd6c8",
+      },
+    ],
+    description:
+      "Hydraulic modeling, forecast inundation maps and consequences estimate support to districts during flood emergencies.",
+  },
+  {
+    id: "608c6afa7f713e19d01ce9f8",
+    index: 8,
+    guid: "272503fe-dc10-4404-9655-6ddc5b149ce0",
+    isActive: true,
+    project: "Innundation Map Viewer",
+    picture: "images/data_viewer.gif",
+    links: [
+      {
+        pointsOfContact:
+          "https://cops.usace.army.mil/sites/HHC/CoPs/DS/MMC/Lists/MMC%20Points%20of%20Contact/AllItems.aspx?InitialTabId=Ribbon%2EListItem&VisibilityContext=WSSTabPersistence",
+        website: "https://fim.sec.usace.army.mil/viewer/",
+      },
+    ],
+    description: "Access inundation maps from MMC-produced hydraulic models.",
+  },
+  {
+    id: "608c6afa95aade4a1e238bc5",
+    index: 9,
+    guid: "727b76ea-7787-49f7-abef-d2c920524273",
+    isActive: true,
+    project: "Program Management",
+    picture: "images/pmtools.png",
+    links: [
+      {
+        pointsOfContact:
+          "https://cops.usace.army.mil/sites/HHC/CoPs/DS/MMC/Lists/MMC%20Points%20of%20Contact/AllItems.aspx?InitialTabId=Ribbon%2EListItem&VisibilityContext=WSSTabPersistence",
+        mmcSiteSearch: "prod.mmc.usace.army.mil/mmc/search",
+        mmcScheduleDatabaseTool: "prod.mmc.usace.army.mil/mmc/scheduledb/",
+        rmcFundingRequest: "https://geo1.mmc.usace.army.mil/frs/",
+        mmcFundingRequest:
+          "https://sajcm.saj.usace.army.mil:8443/fundingrequest/mmc",
+        rmcProgramManagementTool:
+          "https://sajcm.saj.usace.army.mil:8443/fundingrequest/pmgr",
+        workloadProjectionTool: "geo1.mmc.usace.army.mil/wpt/",
+      },
+    ],
+    description:
+      "Provide training and produce dam breach analyses for flood control dams.  Includes numerical modeling using HEC-RAS 5.0 with 2D capability, inundation maps for emergency plans, and consequence estimation.  Screen navigation dams for potential consequences resulting from a breach.",
+  },
+  {
+    id: "608c6afa6e755b73853381b1",
+    index: 10,
+    guid: "2d81ea19-3de8-46c8-8dd7-7935137b0ae6",
+    isActive: true,
+    project: "National Levee Database Support Team",
+    picture: "images/nld.png",
+    links: [
+      {
+        pointsOfContact:
+          "https://cops.usace.army.mil/sites/HHC/CoPs/DS/MMC/Lists/MMC%20Points%20of%20Contact/AllItems.aspx?InitialTabId=Ribbon%2EListItem&VisibilityContext=WSSTabPersistence",
+        nationalLeveeDatabase: "https://levees.sec.usace.army.mil/#/",
+      },
+    ],
+    description:
+      "  Processes district-provided data updates into the national database. Incorporates data provided by other agencies. Provides support for non-routine tasks.",
+  },
+  {
+    id: "608c6afa687c88a90ff9af12",
+    index: 11,
+    guid: "eda57cb1-8ce3-4249-8d73-f5d81fc9fd3a",
+    isActive: true,
+    project: "Project Management",
+    picture: "images/pm.png",
+    links: [
+      {
+        pointsOfContact:
+          "https://cops.usace.army.mil/sites/HHC/CoPs/DS/MMC/Lists/MMC%20Points%20of%20Contact/AllItems.aspx?InitialTabId=Ribbon%2EListItem&VisibilityContext=WSSTabPersistence",
+        newsletter:
+          "https://cops.usace.army.mil/sites/HHC/CoPs/DS/MMC/Shared&Documents/Forms/AllItems.aspx?RootFolder=%2Fsites%2FHHC%2FCoPs%2FDS%2FMMC%2FShared%20Documents%2FMMC%20Newsletter&FolderCTID=0x012000448E47A8D6D89E4C845DEE97DCF4EC77&View=%7bE7B3449D-4ED7-409B-BD37",
+        centerMetrics:
+          "https://cops.usace.army.mil/sites/HHC/CoPs/DS/MMC/Shared&Documents/Forms/AllItems.aspx?RootFolder=%2Fsites%2FHHC%2FCoPs%2FDS%2FMMC%2FShared%20Documents%2FMMC%20Metrics&FolderCTID=0x012000448E47A8D6D89E4C845DEE97DCF4EC77&View=%7bE7B3449D-4ED7-409B-BD37-EF",
+        quarterlyReports:
+          "https://cops.usace.army.mil/sites/HHC/CoPs/DS/MMC/Shared&Documents/Forms/AllItems.aspx?RootFolder=%2Fsites%2FHHC%2FCoPs%2FDS%2FMMC%2FShared%20Documents%2FMonthly%20Status%20Reports&FolderCTID=0x012000448E47A8D6D89E4C845DEE97DCF4EC77&View=%7bE7B3449D-4ED7-4",
+      },
+    ],
+    description:
+      " MMC-dedicated program and project managers serving as single point of coordination and information for USACE programs and districts.",
+  },
+  {
+    id: "609065cd62e981da0841d674",
+    index: 12,
+    guid: "cb28d23f-f7d9-4cb1-8497-353148ca1108",
+    isActive: false,
+    project: "Event Registration",
+    picture: "images/register.png",
+    links: [
+      {
+        pointsOfContact:
+          "https://cops.usace.army.mil/sites/HHC/CoPs/DS/MMC/Lists/MMC%20Points%20of%20Contact/AllItems.aspx?InitialTabId=Ribbon%2EListItem&VisibilityContext=WSSTabPersistence",
+        website: "apps.rgis.dev/event-register",
+      },
+    ],
+    description: "Create and manage meetings, training events and workshops.",
+  },
+  {
+    id: "609065cdea8143915e86d52e",
+    index: 13,
+    guid: "f53721b7-4976-4f60-9147-62bb0d8bada6",
+    isActive: false,
+    project: "Rapid Repsonse Inundation Mapping",
+    picture: "images/RRIM.png",
+    links: [
+      {
+        pointsOfContact: [
+          {
+            sharePoint:
+              "https://cops.usace.army.mil/sites/HHC/CoPs/DS/MMC/Lists/MMC%20Points%20of%20Contact/AllItems.aspx?InitialTabId=Ribbon%2EListItem&VisibilityContext=WSSTabPersistence",
+            cwmsRIMGISLeads:
+              "Ed Stowaser `mailto:+Edward.L.Stowasser@usace.army.mil`, Mike Koon `mailto:+Michael.D.Koon@usace.army.mil`",
+          },
+        ],
+        website:
+          "https://lrh-ws-im-tst.lrh.usace.army.mil/ec/GDMS/webmap/RRIM_dev2/",
+      },
+    ],
+    description:
+      "The Rapid Inundation Mapping (RIM) tool is a web-based GIS tool developed by the MMC to assist water managers during flood events and planning of emergency exercises.  The tool uses a library of stored inundation maps developed for multiple flood profiles with HEC-RAS to help users quickly build an event-specific inundation picture and consequences for an entire watershed.",
+  },
+  {
+    id: "609065cdb5d199faa2f58ca7",
+    index: 14,
+    guid: "d15eca19-516f-4449-819b-56c5ae7ac8da",
+    isActive: false,
+    project: "Event Registration",
+    picture: "images/register.png",
+    links: [
+      {
+        pointsOfContact:
+          "https://cops.usace.army.mil/sites/HHC/CoPs/DS/MMC/Lists/MMC%20Points%20of%20Contact/AllItems.aspx?InitialTabId=Ribbon%2EListItem&VisibilityContext=WSSTabPersistence",
+        website: "https://sajcm.saj.usace.army.mil:8443/register/",
+      },
+    ],
+    description: "Create and manage meetings, training events and workshops.",
+  },
+  {
+    id: "609065cdeccd87ce2e0ede73",
+    index: 15,
+    guid: "4a770ebe-f266-462b-8742-974b77128f7a",
+    isActive: false,
+    project: "Event Registration",
+    picture: "images/register.png",
+    links: [
+      {
+        pointsOfContact:
+          "https://cops.usace.army.mil/sites/HHC/CoPs/DS/MMC/Lists/MMC%20Points%20of%20Contact/AllItems.aspx?InitialTabId=Ribbon%2EListItem&VisibilityContext=WSSTabPersistence",
+        website: "https://sajcm.saj.usace.army.mil:8443/register/",
+      },
+    ],
+    description: "Create and manage meetings, training events and workshops.",
+  },
+  {
+    id: "609065cd3c03d4fe093eb42f",
+    index: 16,
+    guid: "8e3efa0c-46b3-49d0-81eb-46961d5f7e69",
+    isActive: false,
+    project: "Event Registration",
+    picture: "images/register.png",
+    links: [
+      {
+        pointsOfContact:
+          "https://cops.usace.army.mil/sites/HHC/CoPs/DS/MMC/Lists/MMC%20Points%20of%20Contact/AllItems.aspx?InitialTabId=Ribbon%2EListItem&VisibilityContext=WSSTabPersistence",
+        website: "https://sajcm.saj.usace.army.mil:8443/register/",
+      },
+    ],
+    description: "Create and manage meetings, training events and workshops.",
+  },
+  {
+    id: "609065cd9645f39be1d71010",
+    index: 17,
+    guid: "4856c9f0-d30b-424b-a4ca-e6dffa82fab9",
+    isActive: false,
+    project: "Event Registration",
+    picture: "images/register.png",
+    links: [
+      {
+        pointsOfContact: [
+          {
+            sharePoint:
+              "https://cops.usace.army.mil/sites/HHC/CoPs/DS/MMC/Lists/MMC%20Points%20of%20Contact/AllItems.aspx?InitialTabId=Ribbon%2EListItem&VisibilityContext=WSSTabPersistence",
+            "CWMS & RIM GIS Leads":
+              "Ed Stowaser `mailto:+Edward.L.Stowasser@usace.army.mil`, Mike Koon `mailto:+Michael.D.Koon@usace.army.mil`",
+          },
+        ],
+        website: "https://sajcm.saj.usace.army.mil:8443/register/",
+      },
+    ],
+    description: "Create and manage meetings, training events and workshops.",
+  },
+];
